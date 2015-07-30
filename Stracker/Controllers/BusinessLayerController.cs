43dca8f365f6bb182f1766a410c06dc090237289
mlamarca <mlamarc323@gmail.com>
2015-07-30 12:02:43 -0500

    Add missing files

---
+++ Stracker/Controllers/BusinessLayerController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Stracker.Models;
+using Stracker.ViewModel;
+
+namespace Stracker.Controllers
+{
+    public class BusinessLayerController : Controller
+    {
+        private GolfStatTrackerEntities db = new GolfStatTrackerEntities();
+
+        private static JsonSerializerSettings JsonSetting
+        {
+            get
+            {
+                var settings = new JsonSerializerSettings();
+
+                settings.ContractResolver = new DefaultContractResolver()
+                {
+                    IgnoreSerializableAttribute = true,
+                    IgnoreSerializableInterface = true
+                };
+                return settings;
+            }
+        }
+
+        #region Facilities
+        public List<Facility> GetFacilityEntities()
+        {
+            return db.Facilities.ToList();
+        }
+
+        public string GetFacilities()
+        {
+            var facilities = db.Facilities.ToList();
+            var list = new List<Facility>();
+            foreach (Facility f in facilities)
+            {
+                var facilityObj = new Facility();
+                facilityObj.FacilityId = f.FacilityId;
+                facilityObj.Name = f.Name;
+                list.Add(facilityObj);
+            }
+            return JsonConvert.SerializeObject(list, Formatting.Indented, JsonSetting);
+        }
+
+
+        #endregion
+
+        #region Courses
+        public string GetCourses(int facilityId)
+        {
+            var courses = db.Courses.Where(f => f.FacilityId == facilityId).ToList();
+            var list = new List<Cours>();
+            foreach (Cours c in courses)
+            {
+                var courseObj = new Cours();
+                courseObj.CourseId = c.CourseId;
+                courseObj.Name = c.Name;
+                list.Add(courseObj);
+            }
+            return JsonConvert.SerializeObject(list, Formatting.Indented, JsonSetting);
+        }
+
+        public List<Cours> GetCoursesEntitiesByFacility(int facilityId)
+        {
+            var courses = db.Courses.Where(f => f.FacilityId == facilityId).ToList();
+            return courses;
+        }
+
+        #endregion
+
+        #region Tees
+        public string GetTees(int facilityId, int courseId)
+        {
+            var tees = db.Tees.Where(f => f.FacilityId == facilityId && f.CourseId == courseId).ToList();
+            var list = new List<Tee>();
+            foreach (Tee t in tees)
+            {
+                var teeObj = new Tee();
+                teeObj.TeeId = t.TeeId;
+                teeObj.Name = t.Name;
+                list.Add(teeObj);
+            }
+            return JsonConvert.SerializeObject(list, Formatting.Indented, JsonSetting);
+        }
+
+        public List<Tee> GetTeesEntitiesByFacilityAndCourse(int facilityId, int courseId)
+        {
+            var tees = db.Tees.Where(f => f.FacilityId == facilityId && f.CourseId == courseId).ToList();
+            return tees;
+        }
+
+        #endregion
+
+        #region Rounds
+
+        public string GetScorecardDetails(int? roundId)
+        {
+            var roundObj = new Round();
+            roundObj = db.Rounds.First(x => x.RoundId == roundId);
+
+            //var facility = new Facility();
+            //facility = db.Facilities.FirstOrDefault(x => x.FacilityId == roundObj.FacilityId);
+
+            //var course = new Cours();
+            //course = db.Courses.FirstOrDefault(x => x.FacilityId == roundObj.FacilityId &&
+                                                    //x.CourseId == roundObj.CourseId);
+
+            var tees = db.Tees.Where(x => x.FacilityId == roundObj.FacilityId &&
+                                         x.CourseId == roundObj.CourseId &&
+                                         x.TeeId == roundObj.TeeId).ToList();
+
+            //var holes = new List<Hole>();
+            //holes = db.Holes.Where(x => x.FacilityId == roundObj.FacilityId &&
+            //    x.CourseId == roundObj.CourseId &&
+            //    x.TeeId == roundObj.TeeId).ToList();
+
+            //Tuple<Facility, Cours, Tee, List<Hole>> cardDetail =
+            //    new Tuple<Facility, Cours, Tee, List<Hole>>(facility, course, tee, holes);
+
+            var json = JsonConvert.SerializeObject(tees, Formatting.Indented, new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
+
+            return json;
+
+
+        }
+
+        public Scorecard GetScorecardDetailEntity(int? roundId)
+        {
+            var roundObj = new Round();
+            roundObj = db.Rounds.First(x => x.RoundId == roundId);
+
+            var facility = new Facility();
+            facility = db.Facilities.FirstOrDefault(x => x.FacilityId == roundObj.FacilityId);
+
+            var course = new Cours();
+            course = db.Courses.FirstOrDefault(x => x.FacilityId == roundObj.FacilityId &&
+            x.CourseId == roundObj.CourseId);
+
+            var tees = db.Tees.Where(x => x.FacilityId == roundObj.FacilityId &&
+                                         x.CourseId == roundObj.CourseId &&
+                                         x.TeeId == roundObj.TeeId).ToList();
+
+            var holes = new List<Hole>();
+            holes = db.Holes.Where(x => x.FacilityId == roundObj.FacilityId &&
+                x.CourseId == roundObj.CourseId &&
+                x.TeeId == roundObj.TeeId).ToList();
+
+            //Tuple<Facility, Cours, Tee, List<Hole>> cardDetail =
+            //    new Tuple<Facility, Cours, Tee, List<Hole>>(facility, course, tee, holes);
+
+            var scorecard = new Scorecard();
+            scorecard.Round = roundObj;
+            scorecard.Tees = tees;
+            scorecard.Facility = facility;
+            scorecard.Course = course;
+            scorecard.Holes = holes;
+            return scorecard;
+        }
+
+        #endregion
+
+    }
+}
---
+++ Stracker/Controllers/TeesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services;
+using Stracker.Models;
+
+namespace Stracker.Controllers
+{
+    public class TeesController : Controller
+    {
+        private GolfStatTrackerEntities db = new GolfStatTrackerEntities();
+        BusinessLayerController businessLayer = new BusinessLayerController();
+
+        // GET: Tees
+        public ActionResult Index()
+        {
+            return View(db.Tees.ToList());
+        }
+
+        // GET: Tees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tee tee = db.Tees.Find(id);
+            if (tee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tee);
+        }
+
+        // GET: Tees/Create
+        public ActionResult Create()
+        {
+            var facilities = businessLayer.GetFacilityEntities();
+            ViewBag.FacilityId = new SelectList(facilities, "FacilityId", "Name");
+            //ViewBag.CourseId = new SelectList(db.Courses.Where(x=>x.FacilityId == ViewBag.FacilityId), "CourseId", "Name");
+            return View();
+        }
+
+        // POST: Tees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TeeId,FacilityId,CourseId,Name,Slope,Rating,Yardage")] Tee tee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tees.Add(tee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tee);
+        }
+
+        // GET: Tees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tee tee = db.Tees.Find(id);
+            if (tee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tee);
+        }
+
+        // POST: Tees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TeeId,CourseId,Name,Slope,Rating,Yardage")] Tee tee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tee);
+        }
+
+        // GET: Tees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tee tee = db.Tees.Find(id);
+            if (tee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tee);
+        }
+
+        // POST: Tees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tee tee = db.Tees.Find(id);
+            db.Tees.Remove(tee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+    }
+}
